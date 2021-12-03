fc2f55dce349b64e34a2b5ec09c1c89a025226a5
Welpenverschlinger <welpen@verschlin.ger>
2021-12-03 21:37:32 +0100

    adds basic structure for Routing and Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,26 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+//import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthModule
-  ],
-  providers: [],
+    //FormsModule,
+    //ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+  username: String;
+  name: String;
+  email: String;
+  password: String;/**/
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+    ) {
+    this.form = this.formBuilder.group({
+      username: '',
+      name: '',
+      email: '',
+      password: ''
+    })
+    /**/
+    this.username = "";
+    this.name = "";
+    this.email = "";
+    this.password = "";
+   }
+
+  ngOnInit(): void {/* TODO muss das rein?
+    this.form = this.formBuilder.group({
+      username: '',
+      name: '',
+      email: '',
+      password: ''
+    })*/
+  }
+
+  submitForm(){
+    alert("good one uwu");
+    console.log(this.form.getRawValue);/*
+    this.http.post('http://localhost:8080/api/login', this.form.getRawValue(),{withCredentials: true})
+    .subscribe(res => {
+      console.log(res);// TODO remove
+      this.router.navigate(['/']);
+    });
+    
+    const message = `My name is ${this.name}. My email is ${this.email}. My password is ${this.password}. `
+    alert(message);*/
+  }
+
+}
